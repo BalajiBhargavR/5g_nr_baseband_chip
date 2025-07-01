903c52a3f67606237bd095c8b5ff2a67f5bd418b
Balaji Bhargav R <balajibhargavr@gmail.com>
2025-07-02 02:07:34 +0530

    Add initial LDPC encoder RTL

---
+++ rtl/ldpc_encoder.v
@@ -0,0 +1,10 @@
+// rtl/ldpc_encoder.v
+module ldpc_encoder (
+    input wire clk,
+    input wire rst,
+    input wire [N-1:0] data_in,
+    output wire [M-1:0] codeword_out
+);
+    // TODO: Implement LDPC encoding logic here
+
+endmodule
